23c34f7dab6eba0a388f73eecdfcc146704f8626
Rohit Sharma <rohitsharmacse16@gmail.com>
2017-05-05 23:28:53 +0530

    Added fake servicebroker catalog route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dummy App' });
 });
 
+router.get('/v2/catalog', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(require('../catalog.json'));
+});
+
 module.exports = router;
